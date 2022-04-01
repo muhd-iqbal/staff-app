a94a3c22330626447d971abf6414cb0a835ad0a6
Muhamad Iqbal <muhamad_iqbal23@outlook.com>
2022-04-01 15:40:03 +0800

    Add size to receipt, invoice

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Customer;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('customer/{customer}', function ($customer) {
+    return [
+        'order' => Order::with(['order_item'])->where('customer_id', $customer)->where('date', '>=', env('POS_START'))->orderBy('id', 'DESC')->paginate(20),
+        'customer' => Customer::find($customer),
+    ];
 });
