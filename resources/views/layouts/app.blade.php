aa6f889b0bc89cc36cf6c294e0a9a36df25627ae
Muhamad Iqbal <muhamad_iqbal23@outlook.com>
2022-04-29 11:17:01 +0800

    Force browser to load cached js/css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css?v='.filemtime(public_path('css/app.css'))) }}">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js?v='.filemtime(public_path('js/app.js'))) }}"  defer></script>
 
 </head>
 
