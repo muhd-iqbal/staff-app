bfb84f3e69cf59f72f66703609d45b657b7ce0db
Muhamad Iqbal <iqbal@grobox.com.my>
2022-12-19 23:38:24 +0800

    ubah emel ke nama

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
 
             <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('Nama')" />
 
                 <select id="email" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="email" required autofocus>
                     <option selected disabled>Pilih Nama</option>
