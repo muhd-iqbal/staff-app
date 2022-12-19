2c5eaea5149c9f0ef17df7d5d8142635e8f9ecc3
Muhamad Iqbal <iqbal@grobox.com.my>
2022-12-19 23:52:51 +0800

    tukar kembali

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
                 <x-label for="email" :value="__('Nama')" />
 
                 <select id="email" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="email" required autofocus>
-                    <option selected disabled>Senarai Nama</option>
+                    <option selected disabled>Pilih Nama</option>
                     @foreach ($users as $user)
                     <option value="{{ $user->email }}">{{ $user->name }}</option>
                     @endforeach
