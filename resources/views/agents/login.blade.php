25cfc49c56ca9ef47e53b193a9b14cef63c72a7d
Muhamad Iqbal <iqbal@grobox.com.my>
2022-12-19 23:51:17 +0800

    Edit

--- resources/views/agents/login.blade.php
+++ resources/views/agents/login.blade.php
@@ -23,7 +23,7 @@
                 <select id="id"
                     class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                     name="id" required autofocus>
-                    <option selected disabled>Pilih Nama</option>
+                    <option selected disabled>Nama Agen</option>
                     @foreach ($agents as $agent)
                         <option value="{{ $agent->id }}">{{ $agent->name }}</option>
                     @endforeach
