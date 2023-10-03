6bebbd275afc77ce36326066592e714ff7094f91
Amirul Faudzi <127271948+kinatsuko@users.noreply.github.com>
2023-10-03 11:15:48 +0800

    Revert "Update invoice.blade.php"

--- resources/views/invoices/invoice.blade.php
+++ resources/views/invoices/invoice.blade.php
@@ -124,7 +124,7 @@
                         <div>
                             {{ __('WhatsApp / Telefon: ') }} <br />
                             {{ phone_format($order->branch->phone_1) }}
-                            {{ $order->branch->phone_2 ? ' / ' . phone_format(0135303135) : '' }}
+                            {{ $order->branch->phone_2 ? ' / ' . phone_format($order->branch->phone_2) : '' }}
                         </div>
                     </div>
                 </div>
