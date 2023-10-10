45c20115cd3d247a71971e0fff986af955d59601
Amirul Faudzi <127271948+kinatsuko@users.noreply.github.com>
2023-10-10 12:49:33 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
         'Slip Gaji' => '/payslips',
         'Permohonan Cuti' => '/leaves',
         'Tukar Kata Laluan' => '/change-password',
-        'Aktiviti Syarikat' => 'http://localhost/eras/admin/',
+        'Aktiviti Syarikat' => '/http://localhost/eras/admin/',
     ];
     protected $links_staff = [
         'Peti Tunai' => '/cashflow',                
