7dbd9411f1f147a92ae92726c22bc3e54d4a0778
syirah <93442730+asyirahh@users.noreply.github.com>
2023-10-10 12:43:34 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -67,11 +67,10 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/quote/{quote}/print', [QuotationController::class, 'print']);
     Route::patch('/quote/{quote}/print', [QuotationController::class, 'update_footer']);
     Route::post('/quote/{quote}/export', [QuotationController::class, 'export']);
-    Route::get('/quote/item/{item}/edit', [QuotationController::class, 'edit']);
 
     Route::post('/quote/{quote}/add-item', [QuotationItemController::class, 'insert']);
     Route::delete('/quote/{quote}/{list}/delete', [QuotationItemController::class, 'delete']);
-    Route::post('/quote/{quote}/{list}/edit', [QuotationItemController::class, 'edit']);
+    Route::get('/quote/{quote}/{list}/edit', [QuotationItemController::class, 'edit']);
 
     Route::get('/payment/{order}', [OrderController::class, 'print']);
 
