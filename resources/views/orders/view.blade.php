d22e16623158ea28c78925c15f09ce452c9be4b5
Amirul Faudzi <127271948+kinatsuko@users.noreply.github.com>
2023-10-18 15:20:05 +0800

    Update view.blade.php

--- resources/views/orders/view.blade.php
+++ resources/views/orders/view.blade.php
@@ -74,12 +74,12 @@
                                         {{ __('No Telefon: ') . $order->customer->phone }}
                                     </div>
                                     <div class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">
-                                        {{ __('Tarikh pesanan: ') . date('d-M-Y h:i A', strtotime($order->created_at)) }}
+                                        {{ __('Tarikh pesanan: ') . date('d-M-Y (h:i A)', strtotime($order->created_at)) }}
                                     </div>
 
                                     
                                     <div class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">
-                                        {{ __('Tarikh/Masa Mula Design: ') . date('d-M-Y h:i A', strtotime($order->is_design_time)) }}
+                                        {{ __('Tarikh/Masa Mula Design: ') . date('d-M-Y (h:i A)', strtotime($order->is_design_time)) }}
                                     </div>
                                     
                                     
