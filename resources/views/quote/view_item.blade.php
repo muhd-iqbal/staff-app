21ddf58d6ccd2d43374e645bb6fa314b341375ae
syirah <93442730+asyirahh@users.noreply.github.com>
2023-11-10 14:47:06 +0800

    Update view_item.blade.php

--- resources/views/quote/view_item.blade.php
+++ resources/views/quote/view_item.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Quotation') . ': ' . quote_num($item->quote_id) }}
+            {{ quote_num($quote->id) }}
         </h2>
     </x-slot>
 
