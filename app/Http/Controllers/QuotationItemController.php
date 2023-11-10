0d389d14e7a5b3007e59540f94df771d7130d64d
syirah <93442730+asyirahh@users.noreply.github.com>
2023-11-10 12:22:31 +0800

    Update QuotationItemController.php

--- app/Http/Controllers/QuotationItemController.php
+++ app/Http/Controllers/QuotationItemController.php
@@ -44,6 +44,17 @@ class QuotationItemController extends Controller
         return redirect('/quote/' . $quote->id)->with('success', 'Item berjaya padam.');
     }
 
+    public function view (QuotationItem $item)
+    {
+        return view('quote.view_item', [
+            'product' => 'required|max:255',
+            'size' => 'required|max:100',
+            'measurement' => ['max:2', Rule::in(array_keys($this->measurement))],
+            'quantity' => 'required|numeric|min:1',
+            'price' => 'required|min:0|numeric',
+        ]);
+    }
+
     public function update(Quotation $quote, QuotationItem $list)
     {
         $attributes = request()->validate([
