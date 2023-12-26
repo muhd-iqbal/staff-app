4c75867eff246fa3046b08669c3f61f098b06783
Amirul Faudzi <127271948+kinatsuko@users.noreply.github.com>
2023-12-26 12:10:39 +0800

    Revert "Update index.blade.php"

--- resources/views/staff_report/index.blade.php
+++ resources/views/staff_report/index.blade.php
@@ -96,6 +96,7 @@
     <x-dashboard-link />
     <script>
         $(function() {
+            var designers = {!! json_encode($users->pluck('name')) !!};
             var orders = {!! json_encode($users->pluck('order_item')->map->count()) !!};
 
             var colors = ['#39f', '#f90', '#f00', '#FFC0CB', '#0f0', '#BF40BF', '#900', '#f60', '#60f', '#999', '#139f'];
