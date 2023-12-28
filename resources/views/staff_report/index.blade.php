31ec1176fc4bfcf0010c8ef25ce57401960f88a5
syirah <93442730+asyirahh@users.noreply.github.com>
2023-12-28 12:18:53 +0800

    Update index.blade.php

--- resources/views/staff_report/index.blade.php
+++ resources/views/staff_report/index.blade.php
@@ -97,14 +97,13 @@
     <script>
         $(function() {
             var designers = {!! json_encode($users->pluck('name')) !!};
-            var orders = {!! json_encode($users->pluck('order_item')->map->count()) !!};
-
-            var colors = ['#39f', '#f90', '#f00', '#FFC0CB', '#0f0', '#BF40BF', '#900', '#f60', '#60f', '#999', '#139f'];
 
             var datasets = designers.map(function(designer, index) {
+                var orders = {!! json_encode($users->where('name', $designer)->pluck('order_item')->map->count()) !!};
+
                 return {
                     label: designer,
-                    data: [orders[index]],
+                    data: [orders[0]],
                     backgroundColor: colors[index],
                     hoverBackgroundColor: '#fff',
                     borderColor: '#000000',
