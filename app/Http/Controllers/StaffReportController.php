51abac5b6d04f64c11b47e4f1c9ab599593cf4ec
Amirul Faudzi <127271948+kinatsuko@users.noreply.github.com>
2023-12-29 15:58:15 +0800

    Revert "Update StaffReportController.php"

--- app/Http/Controllers/StaffReportController.php
+++ app/Http/Controllers/StaffReportController.php
@@ -18,32 +18,32 @@ class StaffReportController extends Controller
     public function yearly($y)
     {
         $dbData = Order::select(
+            // DB::raw('year(date) as year'),
             DB::raw('month(date) as month'),
-            DB::raw('count(id) as total_designs')
         )
             ->where(DB::raw('date(date)'), '>=', config('app.pos_start'))
             ->where(DB::raw('year(date)'), '=', $y)
             ->groupBy('month')
             ->get();
 
-        $orders = [];
+        // $data = [];
 
-        foreach ($dbData as $row) {
-            $orders[month_name($row->month)] = $row->total_designs;
+        // for ($year = 2022; $year <= now()->format('Y'); $year++) {
+        for ($month = 1; $month <= 12; $month++) {
+            $orders[month_name($month)] = (optional($dbData->first(fn ($row) => $row->month == $month))->totals);
         }
-
-        $users = User::with(['order_item' => function ($query) use ($y) {
-            $query->whereYear('date', $y);
-        }])
-            ->where('position_id', '<>', 1)
-            ->where('active', true)
-            ->has('order_item', '>', 0)
-            ->get();
+        // }
 
         return view('staff_report.index', [
             'order' => $orders,
-            'users' => $users,
-            'year' => $y,
+            'users' => User::with('order_item')
+                ->where('position_id', '<>', 1)
+                ->where('active', true)
+                ->has('order_item', '>', 0)
+                ->get(),
+            'month' => 'required|integer|min:1|max:12',
+            'year' => 'required|integer|min:2020|max:'.date("Y"),
+            'current' => 1,
         ]);
     }
 
