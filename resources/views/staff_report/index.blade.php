25c2a787405e62953e4e30094dbb4a3272b2b44c
syirah <93442730+asyirahh@users.noreply.github.com>
2024-01-02 14:19:30 +0800

    Update index.blade.php

--- resources/views/staff_report/index.blade.php
+++ resources/views/staff_report/index.blade.php
@@ -58,7 +58,7 @@
                         
                     </div>
                     
-                    <div style="width: 300px; height: 300px;">
+                    <div style="width: 600px; height: 600px;">
                         <canvas id="chartContainer"></canvas>
                     </div>
                     <div class="flex flex-row-reverse gap-3 mt-5">
