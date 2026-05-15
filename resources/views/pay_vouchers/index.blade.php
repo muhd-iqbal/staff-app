b66c82a2723beded2a28a7a78b09f20477a30542
Amirul Faudzi <127271948+kinatsuko@users.noreply.github.com>
2026-05-15 16:53:20 +0800

    Add date input field to pay vouchers form

--- resources/views/pay_vouchers/index.blade.php
+++ resources/views/pay_vouchers/index.blade.php
@@ -23,6 +23,8 @@
                         class="mb-2" />
                     <x-form.input name="payee_acc_no" label="No Akaun" value="{{ old('payee_acc_no') }}"
                         class="mb-2" />
+                    <x-form.input name="date" label="Date" type="date" value="{{ old('date') }}"
+                        class="mb-2" />
                     <x-form.input name="due_date" label="Due Date" type="date" value="{{ old('due_date') }}"
                         class="mb-2" />
                     <x-form.input name="payment_method" label="Kaedah Pembayaran" value="{{ old('payment_method') }}"
